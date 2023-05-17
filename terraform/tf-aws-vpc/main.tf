b9172814650494b6f6ac119231842c5021976cce
David Mann <ninja@codingninja.com.au>
2023-05-17 21:02:21 +1000

    Update main.tf

--- terraform/tf-aws-vpc/main.tf
+++ terraform/tf-aws-vpc/main.tf
@@ -14,8 +14,4 @@ provider "random" {
 resource "random_integer" "priority" {
   min = 1
   max = 50000
-  keepers = {
-    # Generate a new integer each time we switch to a new listener ARN
-    listener_arn = var.listener_arn
-  }
 }
