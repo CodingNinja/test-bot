e88dd45e5c1d6924ab693f8f3e55c83a2f635ff3
David Mann <ninja@codingninja.com.au>
2023-05-17 20:02:49 +1000

    Create main.tf

---
+++ terraform/tf-aws-vpc/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.67.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
